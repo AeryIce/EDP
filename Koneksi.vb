1be77c58c0a3582493fc635988d0bfdb88d89e8a
AeryIce <agahariiswarajati@gmail.com>
2020-06-18 10:30:44 +0700

    Change Net Framework

---
+++ Koneksi.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.SqlClient
+
+
+Module Koneksi
+	Public Conn As SqlConnection
+	Public Da As SqlDataAdapter
+	Public Dr As SqlDataReader
+	Public Cmd As SqlCommand
+	Public Ds As DataSet
+
+	Sub Koneksi()
+		Try
+			Conn = New SqlConnection("se")
+		Catch ex As Exception
+
+		End Try
+	End Sub
+
+
+End Module
--- edpLoginForm.Designer.vb
+++ edpLoginForm.Designer.vb
@@ -22,19 +22,151 @@ Partial Class edpLoginForm
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
+		Me.TextBox2 = New System.Windows.Forms.TextBox()
+		Me.Label_statusKoneksi = New System.Windows.Forms.Label()
+		Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
 		Me.SuspendLayout()
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Font = New System.Drawing.Font("Arial Unicode MS", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.Location = New System.Drawing.Point(50, 96)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(205, 28)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "Periplus Indonesia"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label2.Location = New System.Drawing.Point(75, 124)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(150, 13)
+		Me.Label2.TabIndex = 1
+		Me.Label2.Text = "JKT - BDG - BALI - SBY - JGJ "
+		'
+		'TextBox1
+		'
+		Me.TextBox1.Location = New System.Drawing.Point(100, 172)
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(125, 20)
+		Me.TextBox1.TabIndex = 5
+		'
+		'TextBox2
+		'
+		Me.TextBox2.Location = New System.Drawing.Point(100, 214)
+		Me.TextBox2.Name = "TextBox2"
+		Me.TextBox2.Size = New System.Drawing.Size(125, 20)
+		Me.TextBox2.TabIndex = 7
+		'
+		'Label_statusKoneksi
+		'
+		Me.Label_statusKoneksi.AutoSize = True
+		Me.Label_statusKoneksi.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label_statusKoneksi.Location = New System.Drawing.Point(249, 378)
+		Me.Label_statusKoneksi.Name = "Label_statusKoneksi"
+		Me.Label_statusKoneksi.Size = New System.Drawing.Size(0, 9)
+		Me.Label_statusKoneksi.TabIndex = 10
+		'
+		'PictureBox3
+		'
+		Me.PictureBox3.BackgroundImage = Global.EDP.My.Resources.Resources._732c2664fba77085839be9be4e80e4aa
+		Me.PictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+		Me.PictureBox3.Location = New System.Drawing.Point(-1, 12)
+		Me.PictureBox3.Name = "PictureBox3"
+		Me.PictureBox3.Size = New System.Drawing.Size(300, 375)
+		Me.PictureBox3.TabIndex = 11
+		Me.PictureBox3.TabStop = False
+		'
+		'Button2
+		'
+		Me.Button2.BackgroundImage = Global.EDP.My.Resources.Resources.cancel_32px
+		Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+		Me.Button2.Location = New System.Drawing.Point(165, 246)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(44, 39)
+		Me.Button2.TabIndex = 9
+		Me.Button2.UseVisualStyleBackColor = True
+		'
+		'Button1
+		'
+		Me.Button1.BackgroundImage = Global.EDP.My.Resources.Resources.enter_32px
+		Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+		Me.Button1.Location = New System.Drawing.Point(109, 246)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(46, 39)
+		Me.Button1.TabIndex = 8
+		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'PictureBox2
+		'
+		Me.PictureBox2.BackgroundImage = Global.EDP.My.Resources.Resources.password_32px
+		Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+		Me.PictureBox2.Location = New System.Drawing.Point(61, 208)
+		Me.PictureBox2.Name = "PictureBox2"
+		Me.PictureBox2.Size = New System.Drawing.Size(33, 32)
+		Me.PictureBox2.TabIndex = 6
+		Me.PictureBox2.TabStop = False
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.BackgroundImage = Global.EDP.My.Resources.Resources.user_32px
+		Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+		Me.PictureBox1.Location = New System.Drawing.Point(61, 166)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(33, 32)
+		Me.PictureBox1.TabIndex = 4
+		Me.PictureBox1.TabStop = False
 		'
 		'edpLoginForm
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(800, 450)
+		Me.BackColor = System.Drawing.Color.Aqua
+		Me.ClientSize = New System.Drawing.Size(300, 400)
+		Me.Controls.Add(Me.Label_statusKoneksi)
+		Me.Controls.Add(Me.Button2)
+		Me.Controls.Add(Me.Button1)
+		Me.Controls.Add(Me.TextBox2)
+		Me.Controls.Add(Me.PictureBox2)
+		Me.Controls.Add(Me.TextBox1)
+		Me.Controls.Add(Me.PictureBox1)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.PictureBox3)
 		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
 		Me.Name = "edpLoginForm"
+		Me.Opacity = 0.85R
 		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
 		Me.Text = "Login"
+		CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
 		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
 	End Sub
 
+	Friend WithEvents Label1 As Label
+	Friend WithEvents Label2 As Label
+	Friend WithEvents TextBox1 As TextBox
+	Friend WithEvents PictureBox1 As PictureBox
+	Friend WithEvents TextBox2 As TextBox
+	Friend WithEvents PictureBox2 As PictureBox
+	Friend WithEvents Button1 As Button
+	Friend WithEvents Button2 As Button
+	Public WithEvents Label_statusKoneksi As Label
+	Friend WithEvents PictureBox3 As PictureBox
 End Class
