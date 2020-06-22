6bf9fe1ce9ef90f8fabd447af44603a42861ea3c
AeryIce <agahariiswarajati@gmail.com>
2020-06-22 15:28:41 +0700

    utak atik tampilan

--- edpHomeForm.vb
+++ edpHomeForm.vb
@@ -1,6 +1,21 @@
 ﻿Public Class edpHomeForm
-	Private Sub Buttonmaster_MouseEnter(sender As Object, e As EventArgs) Handles Buttonmaster.MouseEnter
 
+
+	Private Sub edpHomeForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		Call KoneksiINV()
+		Call viewBeginning()
 
 	End Sub
+
+	Private Sub Buttonmaster_MouseHover(sender As Object, e As EventArgs) Handles Buttonmaster.MouseHover
+		Call showPanelSubMenuMaster()
+
+	End Sub
+
+	Private Sub Buttonmaster_MouseLeave(sender As Object, e As EventArgs) Handles Buttonmaster.MouseLeave
+		Call hidePanelSubMenuMaster()
+
+	End Sub
+
+
 End Class
